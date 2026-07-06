7a9f1a0106234e3411ee4373396873a4bbb2bf1e
Mahammadalthaf <althaf.syed@nybinfotech.com>
2026-07-06 17:48:57 +0530

    reactjs

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Tailwind CSS is Working perfect! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
