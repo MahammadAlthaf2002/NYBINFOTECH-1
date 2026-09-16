8312de0e812fc427cbe726d9757a3b29ed78d035
Mahammadalthaf <althaf.syed@nybinfotech.com>
2026-09-16 18:44:08 +0530

    js

---
+++ js(16-09-2026)/script.js
@@ -0,0 +1,207 @@
+let products = [
+
+{
+id:1,
+name:"iPhone 16",
+category:"Mobile",
+price:80000,
+stock:10,
+discount:10,
+
+brand:{
+    name:"Apple",
+    country:"USA"
+},
+
+reviews:[
+    {user:"Raj", rating:5},
+    {user:"Amit", rating:4}
+]
+
+},
+
+
+{
+id:2,
+name:"MacBook Pro M4",
+category:"Laptop",
+price:150000,
+stock:5,
+discount:8,
+
+brand:{
+    name:"Apple",
+    country:"USA"
+},
+
+reviews:[
+    {user:"John", rating:5},
+    {user:"Sam", rating:5}
+]
+
+},
+
+
+{
+id:3,
+name:"Samsung Galaxy S25",
+category:"Mobile",
+price:70000,
+stock:20,
+discount:12,
+
+brand:{
+    name:"Samsung",
+    country:"Korea"
+},
+
+reviews:[
+    {user:"Mike", rating:4},
+    {user:"Sara", rating:5}
+]
+
+},
+
+
+{
+id:4,
+name:"Dell XPS 15",
+category:"Laptop",
+price:130000,
+stock:8,
+discount:10,
+
+brand:{
+    name:"Dell",
+    country:"USA"
+},
+
+reviews:[
+    {user:"Alex", rating:4}
+]
+
+},
+
+
+{
+id:5,
+name:"Sony WH-1000XM5",
+category:"Headphones",
+price:30000,
+stock:15,
+discount:15,
+
+brand:{
+    name:"Sony",
+    country:"Japan"
+},
+
+reviews:[
+    {user:"David", rating:5},
+    {user:"Emma", rating:4}
+]
+
+},
+
+
+{
+id:6,
+name:"Logitech MX Master 3",
+category:"Accessories",
+price:10000,
+stock:30,
+discount:5,
+
+brand:{
+    name:"Logitech",
+    country:"Switzerland"
+},
+
+reviews:[
+    {user:"Tom", rating:5}
+]
+
+},
+
+
+{
+id:7,
+name:"iPad Pro",
+category:"Tablet",
+price:90000,
+stock:12,
+discount:7,
+
+brand:{
+    name:"Apple",
+    country:"USA"
+},
+
+reviews:[
+    {user:"Chris", rating:5}
+]
+
+},
+
+
+{
+id:8,
+name:"OnePlus 13",
+category:"Mobile",
+price:60000,
+stock:25,
+discount:10,
+
+brand:{
+    name:"OnePlus",
+    country:"China"
+},
+
+reviews:[
+    {user:"Peter", rating:4},
+    {user:"Lisa", rating:5}
+]
+
+},
+
+
+{
+id:9,
+name:"HP Spectre x360",
+category:"Laptop",
+price:120000,
+stock:7,
+discount:9,
+
+brand:{
+    name:"HP",
+    country:"USA"
+},
+
+reviews:[
+    {user:"Ryan", rating:4}
+]
+
+},
+
+
+{
+id:10,
+name:"Mechanical Gaming Keyboard",
+category:"Accessories",
+price:8000,
+stock:50,
+discount:20,
+
+brand:{
+    name:"Razer",
+    country:"USA"
+},
+
+reviews:[
+    {user:"Mark", rating:5}
+]
+
+}
+
+];
